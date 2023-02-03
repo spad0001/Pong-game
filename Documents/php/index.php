3467545cfcf8c563ff08bdf977d762271624f803
Rienzi Spadin <spad0001@hz.nl>
2023-02-03 20:24:38 +0100

    Booleans creating an unordered list throught PHP and learning about booleans, true or false values

--- Documents/php/index.php
+++ Documents/php/index.php
@@ -1,12 +1,12 @@
 <?php
 
-$animals = [
-        'Elephant',
-        'Deer',
-        'Dog',
-        'Cat',
-        'Lion'
-];
+$task = [
+    'title' => 'Go to the gym',
+    'due' => 'tomorrow',
+    'assigned_to' => 'Me',
+    'completed' => true
+]; // title, due, assigned_to, completed
+
 
 require "index.view.php";
 
--- Documents/php/index.view.php
+++ Documents/php/index.view.php
@@ -6,25 +6,29 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP</title>
-    <style>
-        header {
-            background-color: grey;
-            padding: 2em;
-            text-align: center;
-        }
-
-    </style>
 </head>
 <body>
 
 <header>
 
+    <h1>Task for the Day</h1>
+
     <ul>
-        <?php
-            foreach ($animals as $animal) {
-                echo "<li>$animal</li>";
-        }
-        ?>
+        <li>
+            <strong>Title: </strong> </li><?= $task['title']; ?>
+        </li>
+
+        <li>
+            <strong>Due Date: </strong> </li><?= $task['due']; ?>
+        </li>
+
+        <li>
+            <strong>Assigned to: </strong> </li><?= $task['assigned_to']; ?>
+        </li>
+
+        <li>
+            <strong>Status: </strong> </li><?= $task['completed'] ? 'Complete' : 'Incomplete'; ?>
+        </li>
     </ul>
 
 </header>
