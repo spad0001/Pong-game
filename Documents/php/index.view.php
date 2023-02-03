2d9b3234cbb501bb8d4f7843bdfed6f84554f478
Rienzi Spadin <spad0001@hz.nl>
2023-02-03 21:09:25 +0100

    Conditionals, learned the syntax of a conditional statement

--- Documents/php/index.view.php
+++ Documents/php/index.view.php
@@ -15,19 +15,27 @@
 
     <ul>
         <li>
-            <strong>Title: </strong> </li><?= $task['title']; ?>
+            <strong>Title: </strong> <?= $task['title']; ?>
         </li>
 
         <li>
-            <strong>Due Date: </strong> </li><?= $task['due']; ?>
+            <strong>Due Date: </strong> <?= $task['due']; ?>
         </li>
 
         <li>
-            <strong>Assigned to: </strong> </li><?= $task['assigned_to']; ?>
+            <strong>Assigned to: </strong> <?= $task['assigned_to']; ?>
         </li>
 
         <li>
-            <strong>Status: </strong> </li><?= $task['completed'] ? 'Complete' : 'Incomplete'; ?>
+            <strong>Status: </strong>
+
+            <?php if ($task['completed']) : ?>
+                <span class="icon">&#9989;</span>
+            <?php else : ?>
+                <span class="icon">Incomplete</span>
+
+            <?php endif; ?>
+            
         </li>
     </ul>
 
